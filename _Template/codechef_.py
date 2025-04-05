ab5f08bd663783b24b5d3596f1deb2c9896341c5
Takib Yeasar <mdtakibyeasar@gmail.com>
2025-04-05 19:50:48 +0600

    updated codechef templates

---
+++ _Template/codechef_.py
@@ -0,0 +1,22 @@
+
+# Problem link ===>> 
+
+import sys
+import math
+import bisect
+import heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from functools import lru_cache, reduce
+from itertools import permutations, combinations, accumulate
+from typing import List, Tuple, Dict, Set, Optional, Union, Any
+
+MOD = 10**9 + 7
+INF = float('inf')
+input = sys.stdin.read
+print = sys.stdout.write
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
